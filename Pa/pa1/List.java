4475b4530bab5c81aa109ebe223057a46b32f77e
Christopher Gradwohl <christophergradwohl@eduroam-169-233-216-187.ucsc.edu>
2016-06-27 09:52:41 -0700

    testing and completed methods

--- Pa/pa1/List.java
+++ Pa/pa1/List.java
@@ -6,7 +6,6 @@
 //  ListADT for Lex.java using a doubley linked queue
 //-----------------------------------------------------------------------------
 
-// test
 
 public class List {
 
@@ -41,7 +40,7 @@ public class List {
         back = null;
         cursor = null;
         numElements = 0;
-        index = 0;
+        index = -1;
     }
 
 
@@ -52,24 +51,16 @@ public class List {
 
     // Returns the number of elements in this List.
     // Pre: none
-    int length() {
+    public int length() {
         return numElements;
     }
 
     // If cursor is defined, returns the index of the cursor element,
     // otherwise returns -1.
     // Pre: none
-    int index() {
-        if ( cursor==null ) {
-            index = -1;
-            return index;
-        } else {
-            index = cursor.data;
-            return index;
-        }
+    public int index() {
+        return index;
     }
-
-
 
 
     //-------------------------
@@ -78,30 +69,33 @@ public class List {
 
     // Returns front element.
     // Pre: length()>0
-    int front() {
+    public int front() {
         if( this.length()==0 ){
             throw new RuntimeException(
                 "Queue Error: front() called on empty Queue");
         }
-        return front.next.data; // the .next might not be necessary since front is a "pointer"
+        return front.data;
     }
 
     // Returns back element.
     // Pre: length()>0
-    int back() {
+    public int back() {
         if( this.length()==0 ){
             throw new RuntimeException(
                 "Queue Error: back() called on empty Queue");
         }
-    return back.prev.data; // the .prev might not be necessary since back is a "pointer"
-}
+        return back.data;
+    }
 
     // Returns cursor element.
     // Pre: length()>0, index()>=0
-    int get() {
+    public int get() {
         if( this.length()==0 ){
             throw new RuntimeException(
-                "Queue Error: get() called on empty Queue");
+                "List Error: get() called on empty Queue");
+        }else if( this.index()<0 ){
+            throw new RuntimeException(
+                "List Error: get() called on undefined cursor position");
         }
         return cursor.data;
     }
@@ -109,23 +103,22 @@ public class List {
     // Returns true if this List and L are the same integer
     // sequence. The cursor is ignored in both lists.
     // Pre: none
-    boolean equals(List L);
+    //boolean equals(List L);
 
 
     // ----------------------------------------------------------------------
     // Manipulation procedures
     // ----------------------------------------------------------------------
 
-
-    //-------------------------
-    // MakeEmpty
-    //-------------------------
-
     // Resets this List to its original empty state.
     // Pre: none
-    void clear();
-
-
+    public void clear(){
+        front = null;
+        back = null;
+        cursor = null;
+        numElements = 0;
+        index = -1;
+    }
 
     //-------------------------
     // cursor
@@ -134,24 +127,47 @@ public class List {
     // If List is non-empty, places the cursor under the front element,
     // otherwise does nothing.
     // Pre: none
-    void moveFront();
+    public void moveFront(){
+        if( this.numElements>0 ){
+            cursor = front;
+            index = 0;
+        }
+    }
 
     // If List is non-empty, places the cursor under the back element,
     // otherwise does nothing.
     // Pre: none
-    void moveBack();
+    public void moveBack(){
+        if( numElements>0 ){
+            cursor = back;
+            index = numElements - 1;
+        }
+    }
 
     // If cursor is defined and not at front, moves cursor one step toward
     // front of this List, if cursor is defined and at front, cursor becomes
     // undefined, if cursor is undefined does nothing.
     // Pre: none
-    void movePrev();
+    //void movePrev();
+        //index--;
+
 
     // If cursor is defined and not at back, moves cursor one step toward
     // back of this List, if cursor is defined and at back, cursor becomes
     // undefined, if cursor is undefined does nothing.
     // Pre: none
-    void moveNext();
+    public void moveNext(){
+        if( cursor!=null && cursor!=back ){
+            cursor = cursor.next;
+            index++;
+        }else if( cursor!=null && cursor==back ){
+            cursor = null;
+            index = -1;
+        }else if ( cursor==null ){
+            cursor = null;
+            index = -1;
+        }
+    }
 
 
 
@@ -162,20 +178,78 @@ public class List {
     // Insert new element into this List. If List is non-empty,
     // insertion takes place before front element.
     // Pre: none
-    void prepend(int data);
+    public void prepend(int data){
+        Node C = new Node(data);
+        if( numElements==0 ){
+            front = C;
+            back = C;
+        }else {
+            front.prev = C;
+            C.next = front;
+            front = C;
+        }
+        numElements++;
+    }
 
     // Insert new element into this List. If List is non-empty,
     // insertion takes place after back element.
     // Pre: none
-    void append(int data);
+    //void append(int data);
 
     // Insert new element before cursor.
     // Pre: length()>0, index()>=0
-    void insertBefore(int data);
+    public void insertBefore(int data){
+        if( this.length()==0 ){
+            throw new RuntimeException(
+                "List Error: insertBefore() called on empty list");
+        }else if( this.index()<0 ){
+            throw new RuntimeException(
+                "List Error: insertBefore() called on undefined cursor position");
+        }
+
+        Node C = new Node(data);
+        if( cursor==front ){
+            cursor.prev = C;
+            C.next = cursor;
+            front = C;
+        }else {
+            Node P = cursor.prev;
+
+            P.next = C;
+            cursor.prev = C;
+            C.prev = P;
+            C.next = cursor;
+        }
+        index++;
+        numElements++;
+    }
 
     // Inserts new element after cursor.
     // Pre: length()>0, index()>=0
-    void insertAfter(int data);
+    public void insertAfter(int data){
+        if( this.length()==0 ){
+            throw new RuntimeException(
+                "List Error: insertBefore() called on empty list");
+        }else if( this.index()<0 ){
+            throw new RuntimeException(
+                "List Error: insertBefore() called on undefined cursor position");
+        }
+
+        Node C = new Node(data);
+        if( cursor==back ){
+            cursor.next = C;
+            C.prev = cursor;
+            back = C;
+            numElements++;
+        }else {
+            Node P = cursor.next;
+            cursor.next = C;
+            P.prev = C;
+            C.prev = cursor;
+            C.next = P;
+        }
+        numElements++;
+    }
 
 
 
@@ -187,15 +261,15 @@ public class List {
 
     // Deletes the front element.
     // Pre: length()>0
-    void deleteFront();
+    //void deleteFront();
 
     // Deletes the back element.
     // Pre: length()>0
-    void deleteBack();
+    //void deleteBack();
 
     // Deletes cursor element, making cursor undefined.
     // Pre: length()>0, index()>=0
-    void delete();
+    //void delete();
 
 
     // ----------------------------------------------------------------------
@@ -207,13 +281,13 @@ public class List {
     // representation of this List consisting of a space
     // separated sequence of integers, with front on left.
     // Pre: none
-    public String toString();
+    //public String toString();
 
     // Returns a new List representing the same integer sequence as this
     // List. The cursor in the new list is undefined, regardless of the
     // state of the cursor in this List. This List is unchanged.
     // Pre: none
-    List copy();
+    //List copy();
 
     // The following operation is optional, and may come in handy in some future assignment:
     // Returns a new List which is the concatenation of
@@ -221,6 +295,6 @@ public class List {
     // is undefined, regardless of the states of the cursors
     // in this List and L. The states of this List and L are
     // unchanged.
-    List concat(List L);
+    //List concat(List L);
 
 }
--- Pa/pa1/ListTest.java
+++ Pa/pa1/ListTest.java
@@ -9,6 +9,92 @@
 
 public class ListTest {
     public static void main(String[] args) {
-        
+        // try using and tetsing method with AND without the 'this' keyword to get an intuition on the matter
+
+        List myList = new List();
+
+        System.out.println("empty state: ");
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println();
+
+
+        System.out.println("prepend(7)");
+        myList.prepend(7);
+        myList.moveBack();
+        myList.moveNext();
+        myList.moveNext();
+        myList.moveFront();
+
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("insertBefore(2)");
+        myList.insertBefore(2);
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("moveBack()");
+        myList.moveBack();
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("moveFront()");
+        myList.moveFront();
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("insertAfter(14)");
+        myList.insertAfter(14);
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("moveBack()");
+        myList.moveBack();
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+        System.out.println("get(): " + myList.get());
+        System.out.println("front(): " + myList.front());
+        System.out.println("back(): " + myList.back());
+
+        System.out.println();
+        System.out.println("clear()");
+        myList.clear();
+        System.out.println();
+
+        System.out.println("List Length: " + myList.length());
+        System.out.println("Cursor Index: " + myList.index());
+
     }
 }
